2381f9a160f446240b370e0a997c9a530b94653e
Wilson Chan <wilson_chan0913@hotmail.com>
2023-05-16 15:31:29 +0800

    myPython: Hellow World

---
+++ hello_world.py
@@ -0,0 +1,33 @@
+name = "stranger"
+
+print("Hello", name)
+print("Are you ready to learn Python? Let's go!")
+
+pizza_has_pepperoni = True
+pizza_has_mushrooms = False
+if pizza_has_pepperoni and pizza_has_mushrooms:
+    pizza = "pepperoni and mushrooms"
+elif pizza_has_pepperoni or pizza_has_mushrooms:
+    pizza = "pepperoni or mushrooms"
+print(f"I like pizza with {pizza} toppings!")
+
+mood = input()
+
+if mood == "tired":
+    hit_snooze_button = True
+    print("Adrienne is tired. She hits the snooze button.")
+elif mood == "well-rested":
+    get_out_of_bed = True
+    print("Adrienne is well-rested. She's already out of bed.")
+
+doloresChocolateChips = 13
+teddyChocolateChips = 9
+
+#if teddyChocolateChips > doloresChocolateChips:
+print(f"Teddy's cookie has more chocolate chips than Dolores's. This is {teddyChocolateChips > doloresChocolateChips}!")
+
+name = "Wilson"
+entree = "Pizza"
+print(name + entree)
+
+print("Hello Jupyter")
